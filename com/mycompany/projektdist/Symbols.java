13255640f9558bbacea4e37cea9f7a9295edc54f
VidJoha <vid.joha22@gmail.com>
2026-07-05 01:12:56 +0200

    Add Symbols.java once

---
+++ com/mycompany/projektdist/Symbols.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projektdist;
+
+public class Symbols {
+public static final int Infinity = -1;
+// internet related
+public static final String nameServer = "10.46.109.99";
+public static final int ServerPort = 7033;
+public static final int coordinator = 0;
+// time bounds on messages for synchronous algorithms
+public static final int roundTime = 500; // ms
+public static final boolean debugFlag = true;
+}
